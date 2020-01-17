fd3a4e0ff2f9d9d35bef60cbe7a4e905a613f29d
Renato Groffe <renato.groffe@yahoo.com.br>
2020-01-17 02:56:38 -0300

    Commit via site do GitHub

--- APIContagem/Controllers/ContadorController.cs
+++ APIContagem/Controllers/ContadorController.cs
@@ -25,7 +25,7 @@ namespace APIContagem.Controllers
                     Environment.MachineName,
                     Sistema = Environment.OSVersion.VersionString,
                     Variavel = configuration["TesteAmbiente"],
-                    Local = "Teste",
+                    Local = "Azure Web App / Testes no GitHub",
                     TargetFramework = Assembly
                         .GetEntryAssembly()?
                         .GetCustomAttribute<TargetFrameworkAttribute>()?
